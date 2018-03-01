ff5505235db364bae3f09cd550e99e91ccf342fb
Griffin P Longtin <griflongtin@live.com>
2018-02-28 16:38:34 -0800

    add some css to the user form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import UserLogIn from  './UserLogIn.js';
 import UserSignUp from  './UserSignUp.js';
+let SelectTab = false;
 class UserForm extends Component {
   render() {
     return (
@@ -17,29 +18,34 @@ class UserForm extends Component {
             text-align: center;
           }
           .LOGIN {
-            background-color: var(--color5);
             border-radius: 50px 50px 0 0;
             width: 50%;
             display: inline-block;
             border: 3px solid black;
           }
           .SIGNUP {
-            background-color: var(--color2);
             border-radius: 50px 50px 0 0;
             width: 50%;
             display: inline-block;
             border: 3px solid black;
+          }
+          .SelectedTab {
+            background-color: var(--color2);
+            border: 3px solid black;
             border-bottom-color: var(--color2);
+            border-radius: 50px 50px 0 0;
+            width: 50%;
+            display: inline-block;
           }
           .TabeText {
             padding: 7px 10px;
           }
         `}</style>
         <div className="tabs">
-          <div className="LOGIN">
+          <div className={(SelectTab) ? 'LOGIN' : 'SelectedTab' }>
             <h3 className="TabeText">Log In</h3>
           </div>
-          <div className="SIGNUP">
+          <div className={ (SelectTab) ? 'SelectedTab' : 'none' }>
             <h3 className="TabeText">Sign Up</h3></div>
         </div>
         <UserSignUp />
