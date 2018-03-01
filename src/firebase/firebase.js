f989a5fa6573254b0a4cb6d19da3cf9d825a4e74
Griffin <griflongtin@live.com>
2018-03-01 11:01:33 -0800

    add firebase file for auth setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDJJWxkTD9RSdynjMdDtA5-O9_bDNMmSmY',
+  authDomain: 'commit-engineering.firebaseapp.com',
+  databaseURL: 'https://commit-engineering.firebaseio.com',
+  projectId: 'commit-engineering',
+  storageBucket: 'commit-engineering.appspot.com',
+  messagingSenderId: '190529368964'
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
