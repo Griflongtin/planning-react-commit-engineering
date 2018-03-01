786b68ea41bdb69e86742644f567d13f703d26e2
Griffin P Longtin <griflongtin@live.com>
2018-02-28 16:07:09 -0800

    add user SignUP component

---
+++ src/components/UserSignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UserSignUp extends Component {
+  render() {
+    return (
+      <div>
+        <style jsx>{`
+
+      `}</style>
+      </div>
+    );
+  }
+}
+
+export default UserSignUp;
